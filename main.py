98f67732d1962d22feb4d194e56d21d28e6b870d
Westley <wcblacklock@ucdavis.edu>
2020-03-07 21:23:55 -0700

    Added Main

---
+++ main.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+def main():
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
